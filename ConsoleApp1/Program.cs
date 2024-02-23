10612bb02ebb3cc07f7e40ce5b99fa25868c6684
opilane <opilane@HP-CZC0359NNZ>
2024-02-23 11:27:18 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var primeNumbers = new List<int>();
+            primeNumbers.Add(2);
+            primeNumbers.Add(3);
+            primeNumbers.Add(5);
+            primeNumbers.Add(7);
+            Console.WriteLine("No of elelemts:" + primeNumbers.Count);
+        }
+  
+    }
+  }
+ 
